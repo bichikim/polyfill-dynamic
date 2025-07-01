b832ca22b39e7d61441f555db9be85c25629d9b5
bichikim <bichi@live.co.kr>
2025-07-01 19:35:41 +0900

    update

--- src/v1.ts
+++ src/v1.ts
@@ -13,6 +13,7 @@ router.get('/polyfill.js', async (req, res) => {
   const polyfillImportScript = getPolyfillImportScript(polyfillList)
   const polyfillScript = await getPolyfillScript(polyfillImportScript)
   // todo userAgent 를 키로 하여 polyfillScript 를 캐시하고 있으면 캐시된 스크립트를 반환하도록 해야함
+  // 가능 하다면 cdn 으로 배포하고 있는 스크립트를 사용하도록 해야함
   res.type('application/javascript')
   res.send(polyfillScript)
 })
